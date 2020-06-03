913df49e772bee9d7aacc7b41936a73ba4493831
SamratSahoo <samratsahoo2013@gmail.com>
2020-06-03 17:12:18 -0500

    fixed minor mistakes

--- Week 4 - Logistic Regression/LogisticRegression.py
+++ Week 4 - Logistic Regression/LogisticRegression.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+import sklearn
 from sklearn import model_selection
 from sklearn.datasets import fetch_openml
 import numpy as np
@@ -35,25 +38,28 @@ class LogisticRegression:
             x = self.addIntercept(x)
 
         # Weight initialization
-        theta = np.zeros(x.shape[1])
+        self.theta = np.zeros(x.shape[1])
 
         # Apply Gradient Descent
         for i in range(self.iterations):
-            z = np.dot(x, theta)
+            z = np.dot(x, self.theta)
             h = self.sigmoid(z)
             gradient = np.dot(x.T, (h - y)) / y.size
-            theta -= self.learningRate * gradient
+            self.theta -= self.learningRate * gradient
 
-            z = np.dot(x, theta)
+            z = np.dot(x, self.theta)
             h = self.sigmoid(z)
             loss = self.loss(h, y)
 
-            if self.verbose and i % 10000 == 0:
+            if self.verbose and i % 100 == 0:
                 print("Loss: " + str(loss) + '\n')
+                print("Theta: " + str(self.theta[4]))
 
     def predictProbability(self, x):
-        theta = np.zeros(x.shape[1])
-        return self.sigmoid(np.dot(x, theta))
+        m, n = np.shape(x)
+        x = np.c_[np.ones((m, 1)), x]
+        self.theta = np.zeros((n + 1, 1))
+        return self.sigmoid(np.dot(x, self.theta))
 
     def predict(self, x, threshold=0.5):
         # Predict which category the data belongs
@@ -66,9 +72,10 @@ if __name__ == '__main__':
         np.set_printoptions(threshold=sys.maxsize)
         dataPath = "Data/"
         mnist = fetch_openml('mnist_784', data_home=dataPath)
+        # mnist = sklearn.datasets.load_digits() # Use for Debugging!
 
         # Split Data
-        model = LogisticRegression(verbose=True)
+        model = LogisticRegression(verbose=True, iterations=1000)
         xTrain = mnist.data
         yTrain = (mnist.target != 0)
         xTrain, xTest, yTrain, yTest = model_selection.train_test_split(xTrain, yTrain, train_size=0.65, test_size=0.35,
@@ -79,4 +86,7 @@ if __name__ == '__main__':
         print(preds.mean())  # Prints 1.0 on MNIST
 
     except Exception as e:
+        exceptionType, exceptionObject, exceptionThrowback = sys.exc_info()
+        fileName = os.path.split(exceptionThrowback.tb_frame.f_code.co_filename)[1]
+        print(exceptionType, fileName, exceptionThrowback.tb_lineno)
         print(e)
